219d372cf1b6eed0eff6f3c6a819978d70e7deb9
Arkadiusz Adamski <arkadiusz.adamski@gmail.com>
2021-02-04 18:14:28 +0100

    Skip tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,11 @@
+import os
+
 import pytest
 
 from payments_przelewy24.api import Przelewy24API, Transaction
 from payments_przelewy24.config import Przelewy24Config
+
+SKIP_SANDBOX_TEST = os.getenv("PAYMENTS_P24_POS_ID") is None
 
 
 def test_test_connection_should_pass(config: Przelewy24Config, api, response):
@@ -26,12 +30,18 @@ def test_register_should_pass(
     )
 
 
-@pytest.mark.live
+@pytest.mark.skipif(
+    SKIP_SANDBOX_TEST,
+    reason="P24 sandbox configuration not found",
+)
 def test_live_test_connection_should_pass(api):
     assert api.testConnection()
 
 
-@pytest.mark.live
+@pytest.mark.skipif(
+    SKIP_SANDBOX_TEST,
+    reason="P24 sandbox configuration not found",
+)
 def test_live_test_connection_should_register_transaction(
     api: Przelewy24API, transaction: Transaction
 ):
